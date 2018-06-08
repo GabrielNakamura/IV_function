143c57d2827340dd846658c83f542d88d93618e8
GabrielNakamura <gabriel.nakamura.souza@gmail.com>
2018-06-08 16:33:06 -0300

    arquivo com modificações da função original ImportanceVal.R

---
+++ ImportanceVal_V2.R
@@ -0,0 +1,51 @@
+ImportanceVal<- function(matrix.M, scale= TRUE, method= "standardize", stopRule= TRUE){
+  if(is.matrix(matrix.M) == FALSE){
+    matrix.M<- as.matrix(matrix.M)
+    if(ncol(matrix.M)<3){
+      stop("\n matrix M must be at least 3 components of diversity\n")
+    }
+    if(nrow(matrix.M)<3){
+      stop("\n Matrix M must be at least 3 communities\n")
+    }
+  } 
+  matrix.M.stand<-vegan::decostand(x = matrix.M, method = method, MARGIN = 2)[1:nrow(matrix.M),]
+  if(scale == TRUE){
+    metric.sqrt.corr<- (prcomp(x = matrix.M.stand, scale.= FALSE)$rotation ^ 2)
+    prop.var<- summary(prcomp(x = matrix.M.stand, scale.= FALSE))$importance[2,]
+    IVs.resul<- matrix(nrow= ncol(metric.sqrt.corr), ncol= ncol(matrix.M), dimnames= list(paste("PC",1:ncol(metric.sqrt.corr)), colnames(matrix.M)))
+    for(i in 1:nrow(metric.sqrt.corr)){
+      IVs.resul[,i]<- metric.sqrt.corr[i,] * as.matrix(prop.var)
+    }
+    IVs.proportion<-matrix(nrow= ncol(metric.sqrt.corr), ncol= ncol(matrix.M), dimnames= list(paste("PC",1:ncol(metric.sqrt.corr)), colnames(matrix.M)))
+    for(i in 1:nrow(IVs.resul)){
+      IVs.proportion[i,]<-IVs.resul[i,]/prop.var[i]
+    }
+    if(stopRule==TRUE){
+      sig.eigen<-which(prcomp(matrix.M.stand, scale. = FALSE)$sdev^2>mean(prcomp(matrix.M.stand, scale. = FALSE)$sdev^2))
+      IVs.resul.sig<-IVs.resul[sig.eigen,]
+      IV.resul.sig<- setNames(list(IVs.resul.sig, prop.var, metric.sqrt.corr), c("IV.obs_stopRule", "Var.by.axis","Metrics_correlation"))
+      return(IV.resul.sig)
+    } else{
+      IV.resul<- setNames(list(IVs.resul, prop.var, metric.sqrt.corr), c("IV.obs", "Var.by.axis","Metrics_correlation"))
+      return(IV.resul)
+    }
+  }
+  if(scale == FALSE){
+    metric.sqrt.corr<- (prcomp(x = matrix.M, scale.= FALSE)$rotation ^ 2)
+    prop.var<- summary(prcomp(x = matrix.M, scale.= FALSE))$importance[2,]
+    names.matrix.IV<- list("IV.resu", colnames(matrix.M))
+    IVs.resul<- matrix(nrow= ncol(metric.sqrt.corr), ncol= ncol(matrix.M), dimnames= names.matrix.IV)
+    for(i in 1:nrow(metric.sqrt.corr)){
+      IVs.resul[,i]<- metric.sqrt.corr[i,] * as.matrix(prop.var)
+    }
+    if(stopRule==TRUE){
+      sig.eigen<-which(prcomp(matrix.M, scale. = FALSE)$sdev^2>mean(prcomp(matrix.M, scale. = FALSE)$sdev^2))
+      IVs.resul.sig<-IVs.resul[sig.eigen,]
+      IV.resul.sig<- setNames(list(IVs.resul.sig, prop.var, metric.sqrt.corr), c("IV.obs_stopRule", "Var.by.axis","Metrics_correlation"))
+      return(IV.resul.sig)
+    } else{
+      IV.resul<- setNames(list(IVs.resul, prop.var, metric.sqrt.corr), c("IV.obs_stopRule", "Var.by.axis","Metrics_correlation"))
+      return(IV.resul)
+    }
+  }
+}
